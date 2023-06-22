cc645fcec187e64980e1ff27c35756fde32c9182
FarhanVirenze <farhanvirenze18@gmail.com>
2023-06-22 15:47:49 +0700

    tambahan fungsi catch (exception& e)

--- CircularQueues_139/CircularQueues_139.cpp
+++ CircularQueues_139/CircularQueues_139.cpp
@@ -103,8 +103,51 @@ public:
 };
 
 int main() {
+	Queues q;
+	char ch;
 
+	while (true) {
+		try {
+			cout << "Menu" << endl;
+			cout << "1. Implement insert operation" << endl;
+			cout << "2. Implement delete operation" << endl;
+			cout << "3. Display values" << endl;
+			cout << "4. Exit" << endl;
+			cout << "Enter your choice (1-4): ";
+			cin >> ch;
+			cout << endl;
+
+			switch (ch) {
+			case '1': {
+
+				q.insert();
+				break;
+			}
+			case '2': {
+				q.remove();
+				break;
+			}
+			case '3': {
+				q.display();
+				break;
+			}
+			case '4': {
+				return 0;
+			}
+			default: {
+				cout << "Invalid option!!" << endl;
+				break;
+			}
+			}
+		}
+		catch (exception& e) {
+			cout << "Check for the values entered." << endl;
+		}
+	}
+
+	return 0;
 }
+
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
