d9084cabb028c8af44b148058635f7e246aa6bf8
FarhanVirenze <farhanvirenze18@gmail.com>
2023-06-22 15:12:20 +0700

    tambahan fungsi void remove

--- CircularQueues_139/CircularQueues_139.cpp
+++ CircularQueues_139/CircularQueues_139.cpp
@@ -41,7 +41,9 @@ public:
 		queue_array[REAR] = num;
 	}
 
+	void remove() {
 
+	}
 };
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
